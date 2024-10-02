3ab310c134ddf423c3977326bae964aa92e0a124
Maulaz Tony <tony.maulaz@heig-vd.ch>
2024-10-02 11:07:16 +0200

    First commit

---
+++ code/main.py
@@ -0,0 +1,5 @@
+# Script Python qui utilise une compréhension de liste
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = [x**2 for x in numbers]
+
+print(f"Les carrés des nombres {numbers} sont : {squared_numbers}")
